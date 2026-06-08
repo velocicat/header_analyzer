c525877eac56df72b5fa10a6e263be136d36c0a4
JoshuaH <joshua.d.hein@gmail.com>
2026-06-08 16:00:29 -0400

    add tables for arc results and all headers

--- header_analyzer.py
+++ header_analyzer.py
@@ -1,9 +1,12 @@
 import email
+import re
 from nicegui import ui
 
+re_get_text_in_parents = r'(\(.*?\))'
 def analyze_header():
 
     msg = email.message_from_string(header_text.value)
+    all_headers = msg.items()
 
     # Sender info
     SENDER_NOTES = {
@@ -54,8 +57,11 @@ def analyze_header():
     ui.label("Message Routing")
     ui.table(columns=columns, rows=rows, row_key='name')
 
-    auth_results_header = msg.get('authentication-results').split(';')
+    # Authentication Results display (no ARC handling yet)
+    # TODO: Add ARC result handling
+
     # auth_server = auth_results_header[0]
+    auth_results_header = msg.get('authentication-results').split(';')
     auth_results = auth_results_header[1:]
     
     columns = [
@@ -73,13 +79,69 @@ def analyze_header():
         row = {'method': method, 'result': result, 'detail': auth_result}
         rows.append(row)
 
-    ui.label("Message Results")
+    ui.label("Authentication Results")
     ui.table(columns=columns, rows=rows, row_key='method')
+
+    arc_results = msg.get_all('arc-authentication-results')
+
+    columns = [
+            {'name': 'hop', 'label': 'ARC Hop Counter', 'field': 'hop', 'align': 'left'},
+            {'name': 'server', 'label': 'Authenticating Server', 'field':'server', 'align': 'left'},
+            {'name': 'spf', 'label': 'SPF', 'field': 'spf', 'align': 'left'},
+            {'name': 'dkim', 'label': 'DKIM', 'field': 'dkim', 'align': 'left'},
+            {'name': 'dmarc', 'label': 'DMARC', 'field': 'dmarc', 'align': 'left'},
+            {'name': 'result', 'label': 'ARC Results', 'field': 'result', 'align': 'left'}
+    ]
+
+    rows = []
+    for r in arc_results:
+        result = dict()
+        fields = r.split(';')
+
+        for field in fields:
+            if field.startswith("i"):
+                result["i"] = field.split('=')[-1]
+            if field.find('=') == -1:
+                result["server"] = field.strip()
+            if field.count('=') > 1:
+                match = re.search(re_get_text_in_parents, field)
+                comment = match.group(1) if match else None
+                cleaned_result = re.sub(re_get_text_in_parents, '', field)
+
+                result.update({k: v for item in cleaned_result.split() for k, v in [item.split('=')]})
+
+        row = {
+            'hop': result['i'],
+            'server': result['server'],
+            'spf': result['spf'],
+            'dkim': result['dkim'],
+            'dmarc': result['dmarc'],
+            'result': r
+        }
+
+        rows.append(row)
+
+    ui.label("ARC-Authentication Results")
+    ui.table(columns=columns, rows=rows, row_key='hop')
+
+    # Display all headers
+
+    columns = [
+        {'name': 'name', 'label': 'Header Name', 'field': 'name', 'align': 'left'},
+        {'name': 'value', 'label': 'Header Value', 'field': 'value', 'align': 'left'}
+    ]
+
+    rows = []
+    for header in all_headers:
+        row = {'name': header[0], 'value': header[1]}
+        rows.append(row)
+
+    ui.label("All Message Headers")
+    ui.table(columns=columns, rows=rows, row_key='name')
 
 header_text = ui.textarea(label='Paste Header here', placeholder="paste headers here...")
 ui.button('Analyze Header', on_click=analyze_header)
 
 header_processed = ui.label()
-
 
 ui.run()
